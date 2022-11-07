e20a86854fab364b2553c3b548dac8ff414a85b6
Timo Metsälä <timo.metsala@gmail.com>
2022-11-07 21:57:39 +0200

    Write to InfluxDB and minor fixes here and there

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/timotm/xiaomitemp
 go 1.15
 
 require (
-	github.com/godbus/dbus/v5 v5.0.3
-	github.com/google/btree v1.0.0 // indirect
-	github.com/muka/go-bluetooth v0.0.0-20201211051136-07f31c601d33
+	github.com/godbus/dbus/v5 v5.1.0
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.12.0 // indirect
+	github.com/muka/go-bluetooth v0.0.0-20220830075246-0746e3a1ea53
 	github.com/peterbourgon/diskv v2.0.1+incompatible
-	github.com/posener/goaction v0.1.4
+	github.com/posener/goaction v1.2.1
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/godbus/dbus/v5"
+	"github.com/influxdata/influxdb-client-go/v2"
 	"github.com/muka/go-bluetooth/api"
 	"github.com/muka/go-bluetooth/bluez/profile/adapter"
 	"github.com/muka/go-bluetooth/bluez/profile/device"
@@ -16,14 +19,17 @@ import (
 	"github.com/posener/goaction/log"
 )
 
+// https://github.com/pvvx/ATC_MiThermometer#reading-measurements-in-advertising-mode
+
 type macAddress [6]uint8
 type temperatureAdv struct {
 	Address            macAddress
-	Temperature        uint16
-	HumidityPercent    uint8
-	BatteryPercent     uint8
+	Temperature        int16
+	HumidityPercent    uint16
 	BatteryMilliVolt   uint16
-	FramePacketCounter uint8
+	BatteryPercent     uint8
+	MeasurementCounter uint8
+	Flags              uint8
 }
 
 func handleDiscoveries(discovery <-chan *adapter.DeviceDiscovered, ch chan<- temperatureAdv) {
@@ -81,12 +87,16 @@ func watchDevice(dev *device.Device1, ch chan<- temperatureAdv) error {
 			switch m := ev.Value.(type) {
 			case map[string]dbus.Variant:
 				for key := range m {
+					if !strings.HasPrefix(key, "0000181a") {
+						continue
+					}
+
 					v := m[key].Value()
 					switch vv := v.(type) {
 					case []uint8:
-						temp := temperatureAdv{}
 						buf := bytes.NewReader(vv)
-						err := binary.Read(buf, binary.BigEndian, &temp)
+						temp := temperatureAdv{}
+						err = binary.Read(buf, binary.LittleEndian, &temp)
 						if err != nil {
 							log.Warnf("binary.Read error: %s\n", err)
 						} else {
@@ -107,20 +117,44 @@ func watchDevice(dev *device.Device1, ch chan<- temperatureAdv) error {
 
 func nameFromMac(address macAddress, nameStore *diskv.Diskv) string {
 
-	macString := strings.Trim(strings.Replace(fmt.Sprint(address), " ", "-", -1), "[]")
+	macString := fmt.Sprintf("%02x-%02x-%02x-%02x-%02x-%02x", address[5], address[4], address[3],
+		address[2], address[1], address[0])
 	prettyName, err := nameStore.Read(macString)
 
 	if err != nil {
+		nameStore.Write(macString, []byte(fmt.Sprintf("Sensor %s", macString)))
 		return macString
 	}
 
-	return string(prettyName)
+	return strings.TrimSpace(string(prettyName))
 }
 
-func readTemperatures(ch <-chan temperatureAdv, nameStore *diskv.Diskv) {
+func readAndSendTemperatures(ch <-chan temperatureAdv, nameStore *diskv.Diskv, url string, database string, token string) {
+	client := influxdb2.NewClientWithOptions(url, token, influxdb2.DefaultOptions().SetBatchSize(20).SetLogLevel(0))
+
+	writeAPI := client.WriteAPI("", database)
+	defer writeAPI.Flush()
+	defer client.Close()
+
 	for t := range ch {
 		name := nameFromMac(t.Address, nameStore)
 		fmt.Printf("Got temp %s / %+v\n", name, t)
+		p := influxdb2.NewPoint(
+			"temperature",
+			map[string]string{
+				"room": name,
+			},
+			map[string]interface{}{
+				"temperature":        float64(t.Temperature) / 100.0,
+				"batteryMv":          int(t.BatteryMilliVolt),
+				"humidity":           float64(t.HumidityPercent) / 100.0,
+				"batteryPercent":     int(t.BatteryPercent),
+				"flags":              int(t.Flags),
+				"measurementCounter": int(t.MeasurementCounter),
+			},
+			time.Now())
+		// write asynchronously
+		writeAPI.WritePoint(p)
 	}
 }
 
@@ -129,6 +163,11 @@ func main() {
 		BasePath:     "/var/lib/temperature/",
 		CacheSizeMax: 100 * 1024,
 	})
+
+	dbUrl := flag.String("url", "http://localhost:8086", "InfluxDB URL")
+	dbToken := flag.String("token", "", "InfluxDB token")
+	dbDatabase := flag.String("database", "temperature", "InfluxDB database")
+	flag.Parse()
 
 	sigCh := make(chan os.Signal)
 	signal.Notify(sigCh, os.Interrupt, os.Kill)
@@ -141,7 +180,7 @@ func main() {
 
 	defer btCancel()
 
-	go readTemperatures(temperatureCh, nameStore)
+	go readAndSendTemperatures(temperatureCh, nameStore, *dbUrl, *dbDatabase, *dbToken)
 
 	sig := <-sigCh
 	fmt.Printf("Received signal [%v], shutting down\n", sig)
